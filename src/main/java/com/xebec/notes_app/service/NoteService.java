c158e3d9567493b82cda62a60b85350923c40f62
Glenn Rogers <glennrogers186@gmail.com>
2025-12-28 17:30:50 +0000

    add NoteService interface

--- src/main/java/com/xebec/notes_app/service/NoteService.java
+++ src/main/java/com/xebec/notes_app/service/NoteService.java
@@ -1,63 +1,17 @@
 package com.xebec.notes_app.service;
 
 import com.xebec.notes_app.dto.NoteDto;
-import com.xebec.notes_app.exception.ResourceNotFoundException;
-import com.xebec.notes_app.model.Note;
-import com.xebec.notes_app.model.User;
-import com.xebec.notes_app.repository.NoteRepository;
-import com.xebec.notes_app.repository.UserRepository;
-import lombok.AllArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-@AllArgsConstructor
-public class NoteService {
+public interface NoteService {
+    public NoteDto addNote(NoteDto note);
 
-    private NoteRepository noteRepository;
-    private UserRepository userRepository;
-    private ModelMapper modelMapper;
+    public NoteDto getNoteById(Long id);
 
-    public NoteDto addNote(NoteDto noteDto) {
-        Note note = modelMapper.map(noteDto, Note.class);
-        Note addedNote = noteRepository.save(note);
-        return modelMapper.map(addedNote, NoteDto.class);
-    }
+    public List<NoteDto> getAllNotesByUserId(Long userId);
 
-    public NoteDto getNoteById(Long id) {
-        Note note = noteRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Note does not exist with id: " + id));
-        return modelMapper.map(note, NoteDto.class);
-    }
+    public NoteDto updateNote(Long id, NoteDto noteDto);
 
-    public List<NoteDto> getAllNotesByUserId(Long userId) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new ResourceNotFoundException("User does not exist with id: " + userId));
-        List<Note> notes = noteRepository.findAllNotesByUser(user);
-
-        return notes.stream()
-                .map((note) -> modelMapper.map(note, NoteDto.class))
-                .toList();
-    }
-
-    public NoteDto updateNote(Long id, NoteDto noteDto) {
-        Note note = noteRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Note does not exist with id: " + id));
-
-        note.setContent(noteDto.getContent());
-        note.setTitle(noteDto.getTitle());
-
-        Note updatedNote = noteRepository.save(note);
-        return modelMapper.map(updatedNote, NoteDto.class);
-    }
-
-    public String deleteNote(Long id) {
-        Note note = noteRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Note does not exist with id: " + id));
-
-        noteRepository.delete(note);
-        return "Note deleted successfully";
-    }
+    public String deleteNote(Long id);
 }
---
+++ src/main/java/com/xebec/notes_app/service/impl/NoteServiceImpl.java
@@ -0,0 +1,64 @@
+package com.xebec.notes_app.service.impl;
+
+import com.xebec.notes_app.dto.NoteDto;
+import com.xebec.notes_app.exception.ResourceNotFoundException;
+import com.xebec.notes_app.model.Note;
+import com.xebec.notes_app.model.User;
+import com.xebec.notes_app.repository.NoteRepository;
+import com.xebec.notes_app.repository.UserRepository;
+import com.xebec.notes_app.service.NoteService;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class NoteServiceImpl implements NoteService {
+
+    private NoteRepository noteRepository;
+    private UserRepository userRepository;
+    private ModelMapper modelMapper;
+
+    public NoteDto addNote(NoteDto noteDto) {
+        Note note = modelMapper.map(noteDto, Note.class);
+        Note addedNote = noteRepository.save(note);
+        return modelMapper.map(addedNote, NoteDto.class);
+    }
+
+    public NoteDto getNoteById(Long id) {
+        Note note = noteRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Note does not exist with id: " + id));
+        return modelMapper.map(note, NoteDto.class);
+    }
+
+    public List<NoteDto> getAllNotesByUserId(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User does not exist with id: " + userId));
+        List<Note> notes = noteRepository.findAllNotesByUser(user);
+
+        return notes.stream()
+                .map((note) -> modelMapper.map(note, NoteDto.class))
+                .toList();
+    }
+
+    public NoteDto updateNote(Long id, NoteDto noteDto) {
+        Note note = noteRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Note does not exist with id: " + id));
+
+        note.setContent(noteDto.getContent());
+        note.setTitle(noteDto.getTitle());
+
+        Note updatedNote = noteRepository.save(note);
+        return modelMapper.map(updatedNote, NoteDto.class);
+    }
+
+    public String deleteNote(Long id) {
+        Note note = noteRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Note does not exist with id: " + id));
+
+        noteRepository.delete(note);
+        return "Note deleted successfully";
+    }
+}
