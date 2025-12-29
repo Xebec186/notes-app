8df3e76b108125e770fb5481bf0ca3bb8c3261e6
Glenn Rogers <glennrogers186@gmail.com>
2025-12-29 10:04:09 +0000

    feat: view individual note

--- src/main/java/com/xebec/notes_app/controller/NoteController.java
+++ src/main/java/com/xebec/notes_app/controller/NoteController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -17,13 +18,24 @@ public class NoteController {
 
     private NoteService noteService;
 
+    @ModelAttribute("username")
+    public String username() {
+        return "Glenn";
+    }
+
     @GetMapping("/notes")
-    public String notes(Model model) {
+    public String showNotes(Model model) {
         Long userId = 1L;
         List<NoteDto> notes = noteService.getAllNotesByUserId(userId);
-        model.addAttribute("username", "glenn");
         model.addAttribute("notes", notes);
         return "notes";
+    }
+
+    @GetMapping("/notes/{id}")
+    public String showNote(@PathVariable Long id, Model model) {
+        NoteDto noteDto = noteService.getNoteById(id);
+        model.addAttribute("note", noteDto);
+        return "note";
     }
 
     @GetMapping("/add-note")
