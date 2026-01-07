f773a0a148878ea8e53bd1a79e3b161a808c60cb
Glenn Rogers <glennrogers186@gmail.com>
2026-01-07 19:12:54 +0000

    security: register user with rest api

--- src/main/java/com/xebec/notes_app/controller/api/AuthApiController.java
+++ src/main/java/com/xebec/notes_app/controller/api/AuthApiController.java
@@ -1,7 +1,11 @@
 package com.xebec.notes_app.controller.api;
 
+import com.xebec.notes_app.dto.RegisterDto;
 import com.xebec.notes_app.service.UserService;
+import com.xebec.notes_app.service.impl.JwtService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthApiController {
 
     private UserService userService;
+    private JwtService jwtService;
 
-
+    @PostMapping("register")
+    public String registerUser(@RequestBody RegisterDto registerDto) {
+        userService.register(registerDto);
+        return jwtService.generateToken(registerDto.getUsername());
+    }
 }
